16ec6ed5571f91aa4f13fe96ff435f1537ca72c9
Joakim Bergman <joab@kth.se>
2011-07-13 21:10:47 +0200

    Works even better!

--- MongoStress.java
+++ MongoStress.java
@@ -1,5 +1,3 @@
-package com.videoplaza.tracker.persist;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -56,4 +54,3 @@ public class MongoStress {
       }
    }
 }
-
